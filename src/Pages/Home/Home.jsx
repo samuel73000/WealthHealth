60a98b25c8d74bdcb5c5ed2411516550a77189be
Samuel Pouard <thosam73000@gmail.com>
2025-01-18 20:45:58 +0100

    WeallthHealth

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+import reactLogo from '../../assets/react.svg'
+import viteLogo from '../../../public/vite.svg'
+import './Home.css'
+ function Home() {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +31,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
