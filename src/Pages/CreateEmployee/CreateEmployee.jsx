ccc523245b3dd79da655edad9d724213e8b10a42
Samuel Pouard <thosam73000@gmail.com>
2025-01-21 09:26:50 +0100

    whealthHealth

--- src/Pages/CreateEmployee/CreateEmployee.jsx
+++ src/Pages/CreateEmployee/CreateEmployee.jsx
@@ -1,13 +1,24 @@
 import "./CreateEmployee.css";
-import {TextField} from "@mui/material";
+import { TextField } from "@mui/material";
 export default function CreateEmployee() {
   return (
-    <main>
+    <main className="main-create-employee">
+      <section className="container-1-create-employee">
       <h2>Create Employee</h2>
-      <div>
-      <TextField id="outlined-basic" label="First Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Last Name" variant="outlined" />
-      </div>
+        <TextField id='outlined-basic' label='First Name' variant='outlined' />
+        <TextField id='outlined-basic' label='Last Name' variant='outlined' />
+        <TextField id='outlined-basic' label='Date of Birth' variant='outlined'/>
+        <TextField id='outlined-basic'label='Start Date'variant='outlined'/>
+      </section>
+<section className="container-2-create-employee">
+<fieldset className="container-2-create-employee">
+<legend>Address</legend>
+<TextField id='outlined-basic' label='Street' variant='outlined' />
+<TextField id='outlined-basic' label='City' variant='outlined' />
+<TextField id='outlined-basic' label='State' variant='outlined' />
+<TextField id='outlined-basic' label='Zip Code' variant='outlined' />
+</fieldset>
+</section>
     </main>
   );
 }
