e562fe6f2ed11e98f21d6da22658a12768bbda47
Samuel Pouard <thosam73000@gmail.com>
2025-01-21 11:30:42 +0100

    WealthHealth

--- src/Pages/CreateEmployee/CreateEmployee.jsx
+++ src/Pages/CreateEmployee/CreateEmployee.jsx
@@ -1,24 +1,69 @@
 import "./CreateEmployee.css";
 import { TextField } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import * as React from "react";
+import States from "../../data/States";
+
+
 export default function CreateEmployee() {
+  const [Department, setDepartment] = React.useState("");
+  const [State, setState] = React.useState("");
+  const handleChangeDepartment = (e, SelectChangeEvent) => {
+    setDepartment(e.target.value);
+  };
+  const handleChangeState = (e, SelectChangeEvent) => {
+    setState(e.target.value);
+  };
+
   return (
-    <main className="main-create-employee">
-      <section className="container-1-create-employee">
-      <h2>Create Employee</h2>
+    <main className='main-create-employee'>
+      <form className='container-create-employee'>
+        <h2>Create Employee</h2>
         <TextField id='outlined-basic' label='First Name' variant='outlined' />
         <TextField id='outlined-basic' label='Last Name' variant='outlined' />
-        <TextField id='outlined-basic' label='Date of Birth' variant='outlined'/>
-        <TextField id='outlined-basic'label='Start Date'variant='outlined'/>
-      </section>
-<section className="container-2-create-employee">
-<fieldset className="container-2-create-employee">
-<legend>Address</legend>
-<TextField id='outlined-basic' label='Street' variant='outlined' />
-<TextField id='outlined-basic' label='City' variant='outlined' />
-<TextField id='outlined-basic' label='State' variant='outlined' />
-<TextField id='outlined-basic' label='Zip Code' variant='outlined' />
-</fieldset>
-</section>
+        <TextField
+          id='outlined-basic'
+          label='Date of Birth'
+          variant='outlined'
+        />
+        <TextField id='outlined-basic' label='Start Date' variant='outlined' />
+      </form>
+      <form className='container-create-employee'>
+        <fieldset className='container-create-employee'>
+          <legend>Address</legend>
+          <TextField id='outlined-basic' label='Street' variant='outlined' />
+          <TextField id='outlined-basic' label='City' variant='outlined' />
+
+          <FormControl sx={{ m: 1, minWidth: 250 }}>
+            <InputLabel>State</InputLabel>
+            <Select value={State} label='State' onChange={handleChangeState} >
+              {States.map((States, index) => (
+                <MenuItem value={index}>{States.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <TextField id='outlined-basic' label='Zip Code' variant='outlined' />
+        </fieldset>
+      </form>
+      <form className='container-create-employee'>
+        <FormControl sx={{ m: 1, minWidth: 250 }}>
+          <InputLabel>Department</InputLabel>
+          <Select
+            value={Department}
+            label='Department'
+            onChange={handleChangeDepartment}>
+            <MenuItem value={10}>Sales</MenuItem>
+            <MenuItem value={20}>Marketing</MenuItem>
+            <MenuItem value={40}>Engineering</MenuItem>
+            <MenuItem value={50}>Human Resources</MenuItem>
+            <MenuItem value={60}>Legal</MenuItem>
+          </Select>
+        </FormControl>
+      </form>
     </main>
   );
 }
