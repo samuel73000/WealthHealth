7aa6048b5a3b97e67140ab91b4d48a99b669fcb6
Samuel Pouard <thosam73000@gmail.com>
2025-01-23 22:34:28 +0100

    hrnet

--- src/Components/DateTimePicker/DateTimePicker.jsx
+++ src/Components/DateTimePicker/DateTimePicker.jsx
@@ -1,82 +1,55 @@
 import "./DateTimePicker.css";
 import * as React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretLeft ,faCaretRight ,faHouse } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCaretLeft,
+  faCaretRight,
+  faHouse,
+  faSortDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function DateTimePicker() {
+    
+  function getDaysInMonth(year, month) {
+    const days = [];
+    const date = new Date(year, month, 1); // Premier jour du mois
 
-return(
-    <section className="container-dateTimePicker">
-        <div className="header-dateTimePicker">
+    while (date.getMonth() === month) {
+      days.push(new Date(date)); // Ajouter le jour au tableau
+      date.setDate(date.getDate() + 1); // Passer au jour suivant
+    }
+
+    return days;
+  }
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+
+  const days = getDaysInMonth(year, month);
+  const currentDay = today.getDate();
+
+  return (
+    <section className='container-dateTimePicker'>
+      <div className='header-dateTimePicker'>
         <FontAwesomeIcon icon={faCaretLeft} />
         <FontAwesomeIcon icon={faHouse} />
-        <p>janvier</p>
-        <p>2025</p>
+        <p>{today.toLocaleString("default", { month: "long" })} <FontAwesomeIcon icon={faSortDown} /></p>
+        <p>{today.getFullYear()} <FontAwesomeIcon icon={faSortDown} /></p>
         <FontAwesomeIcon icon={faCaretRight} />
-        </div>
-        <div className="container-days-dateTimePicker" id="container-days-dateTimePicker-1">
-            <p className="days-dateTimePicker">Dim</p>
-            <p className="days-dateTimePicker">Lun</p>
-            <p className="days-dateTimePicker">Mar</p>
-            <p className="days-dateTimePicker">Mer</p>
-            <p className="days-dateTimePicker">Jeu</p>
-            <p className="days-dateTimePicker">Ven</p>
-            <p className="days-dateTimePicker">Sam</p>
-        </div>
-
-        <div className="container-days-dateTimePicker">
-            <p className="days-dateTimePicker">1</p>
-            <p className="days-dateTimePicker">2</p>
-            <p className="days-dateTimePicker">3</p>
-            <p className="days-dateTimePicker">4</p>
-            <p className="days-dateTimePicker">5</p>
-            <p className="days-dateTimePicker">6</p>
-            <p className="days-dateTimePicker">7</p>
-        </div>
-
-        <div className="container-days-dateTimePicker">
-            <p className="days-dateTimePicker">8</p>
-            <p className="days-dateTimePicker">9</p>
-            <p className="days-dateTimePicker">10</p>
-            <p className="days-dateTimePicker">11</p>
-            <p className="days-dateTimePicker">12</p>
-            <p className="days-dateTimePicker">13</p>
-            <p className="days-dateTimePicker">14</p>
-        </div>
-
-        <div className="container-days-dateTimePicker">
-            <p className="days-dateTimePicker">15</p>
-            <p className="days-dateTimePicker">16</p>
-            <p className="days-dateTimePicker">17</p>
-            <p className="days-dateTimePicker">18</p>
-            <p className="days-dateTimePicker">19</p>
-            <p className="days-dateTimePicker">20</p>
-            <p className="days-dateTimePicker">21</p>
-        </div>
-
-        <div className="container-days-dateTimePicker">
-            <p className="days-dateTimePicker">22</p>
-            <p className="days-dateTimePicker">23</p>
-            <p className="days-dateTimePicker">24</p>
-            <p className="days-dateTimePicker">25</p>
-            <p className="days-dateTimePicker">26</p>
-            <p className="days-dateTimePicker">27</p>
-            <p className="days-dateTimePicker">28</p>
-        </div>
-
-        <div className="container-days-dateTimePicker">
-            <p className="days-dateTimePicker">29</p>
-            <p className="days-dateTimePicker">30</p>
-            <p className="days-dateTimePicker">31</p>
-            <p className="days-dateTimePicker">1</p>
-            <p className="days-dateTimePicker">2</p>
-            <p className="days-dateTimePicker">3</p>
-            <p className="days-dateTimePicker">4</p>
-        </div>
-
-
+      </div>
+      <div className='container-days-dateTimePicker '>
+        {days.map((day) => (
+          <p
+            className='days-dateTimePicker'
+            key={day.toISOString()}
+            style={{
+              background: day.getDate() === currentDay ? "lightblue" : "white",
+            }}>
+            {day.getDate()}
+          </p>
+        ))}
+      </div>
     </section>
-)
-
+  );
 }
