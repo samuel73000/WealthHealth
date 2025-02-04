adaca9346e8ecf153354751293f2307e33926919
Samuel Pouard <thosam73000@gmail.com>
2025-02-04 22:20:54 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,13 @@ import "./Home.css";
 
 export default function Home() {
     return (
-        <div className="home">hello world</div>
+        <section className="section-home">
+            <img src="" alt="" />
+        </section>
     )
 }
+// UTILISER RECHARTS POUR FAIRT DES GRAPHIQUES POUR LES STASTIQUES
+// Nombre total d'employés
+// Nombre de nouvelles embauches ce mois-ci
+// Nombre de départs récents
+// Moyenne d'ancienneté des employés 
